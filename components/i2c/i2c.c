637fc562834ef57c3cb146f3d132c8c5881905ec
Felix Schladt <felix.schladt@hensoldt.net>
2024-02-22 14:41:54 +0100

    [DOC] Update Copyright
    Change files to GPLv2 or Commercial licensing

--- components/i2c/i2c.c
+++ components/i2c/i2c.c
@@ -1,7 +1,11 @@
 /*
  * Sabre Lite (i.MX6) I2C driver
  *
- * Copyright (C) 2021, HENSOLDT Cyber GmbH
+ * Copyright (C) 2021-2024, HENSOLDT Cyber GmbH
+ * 
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
  */
 
 #include "OS_Error.h"
